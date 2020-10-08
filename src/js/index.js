f5263bf5dace4c4ec0ffe490547dadd3a0fe6be9
rmisca <rominamisca95@gmail.com>
2020-10-08 22:51:30 +0300

    1. For long recipe title show elipsis, 2. Show the list of recipes which are searched, 3. Clear input after clicking the search button

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,34 @@
 import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import {elements} from "./views/base";
 
-const search = new Search("pizza");
-console.log(search);
-search.getResults();
+/** Global state of the app
+ *  -Search object
+ *  -Current recipe object
+ *  -Shopping list object
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = searchView.getInput();
+
+    if(query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        //3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        //4) Search for recipes
+        await state.search.getResults();
+        //5) Render results on UI
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener(
+    "submit", e => {
+        e.preventDefault();
+        controlSearch();
+    });
