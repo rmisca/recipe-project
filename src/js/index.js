ecc7f08fef915b582e31d77dd036c714c93049d8
rmisca <rominamisca95@gmail.com>
2020-10-09 18:14:18 +0300

    1. Added loading icon then data is fetched, 2. Show only 10 recipes titles on the page, 3. Created buttons for next and previous pages

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from "./models/Search";
 import * as searchView from "./views/searchView";
-import {elements} from "./views/base";
+import {elements, renderLoader, clearLoader} from "./views/base";
+
 
 /** Global state of the app
  *  -Search object
@@ -20,9 +21,13 @@ const controlSearch = async () => {
         //3) Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
+
         //4) Search for recipes
         await state.search.getResults();
+
         //5) Render results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 }
@@ -32,3 +37,14 @@ elements.searchForm.addEventListener(
         e.preventDefault();
         controlSearch();
     });
+
+
+elements.searchResPages.addEventListener("click", e => {
+    const btn = e.target.closest(".btn-inline");
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+
+    }
+})
